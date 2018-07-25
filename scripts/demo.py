924af3c1d585a050d7fecbeeb5f485d7058a1860
jiangyonghui <jasonniee@hotmail.com>
2018-07-25 14:10:58 +0200

    modify demo

--- scripts/demo.py
+++ scripts/demo.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 import torch
-from torch.utils.serialization import load_lua
 import cv2
+import glob
 import numpy as np
 from pose_3d_ros.tools.opts import opts
 from pose_3d_ros.tools import ref
 from pose_3d_ros.utils.debugger import Debugger
 from pose_3d_ros.utils.eval import getPreds
 from pose_3d_ros.utils.img import Crop
-
-import time
 
 def main():
   opt = opts().parse()
@@ -18,8 +16,7 @@ def main():
     model = torch.load(opt.loadModel).cuda()
   else:
     model = torch.load('../models/hgreg-3d.pth').cuda()
-    
-  img = cv2.imread(opt.demo)
+  
   if img.shape != (256,256,3):
     h, w = img.shape[0], img.shape[1]
     center = torch.FloatTensor((w/2, h/2))
@@ -33,7 +30,6 @@ def main():
   output = model(input_var)
   pred = getPreds((output[-2].data).cpu().numpy())[0] * 4
   reg = (output[-1].data).cpu().numpy().reshape(pred.shape[0], 1)
-  print reg
   pose2D = pred
   pose3D = np.concatenate([pred, (reg+1) / 2. * 256], axis = 1)
   print "3D Pose:\n", pose3D
@@ -44,7 +40,17 @@ def main():
   debugger.addPoint3D(pose3D)
   #debugger.saveImg("debug_1_0.png")
   debugger.showImg(pause = True)
-  debugger.show3D()
+  debugger.show3D()   
+    
 
 if __name__ == '__main__':
   main()
+  
+  
+  
+  
+  
+
+
+
+
